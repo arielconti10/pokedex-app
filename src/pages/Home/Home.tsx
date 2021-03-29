947206ae37ea2f4309527a531fdad053110e2390
Ariel Conti <arielconti10@gmail.com>
2021-03-29 19:09:29 -0300

    eat: created first list and some helper function

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-
-
+import { FlatList, Image, Text, View } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { capitalize } from '../../helpers';
+import api from '../../services/api';
 
 import { Container, SearchBar, Title } from './styles';
 
+interface Pokemon{
+  name: string,
+  url: string
+}
+
 const Home: React.FC = () => {
   const [searchPokemon, setSearchPokemon] = useState<string>('');
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
-  const handleSearchPokemon = () => {
+  useEffect(() => {
+    api.get('/pokemon?limit=10').then(
+      (result) => {
+        setPokemonList(result.data.results);
+      }
+    );
+  }, [])
 
-  }
+
+  const renderPokemon = (result: { item: Pokemon }) => (
+    <TouchableOpacity style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center'
+    }}>
+      <Image
+          style={{width: 80, height: 80}}
+          source={{
+            uri: `https://img.pokemondb.net/sprites/home/normal/${
+              result.item.name
+            }.png`,
+          }}
+        />
+      <Text>{capitalize(result.item.name)}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <Container>
@@ -21,10 +50,13 @@ const Home: React.FC = () => {
         <TextInput style={{color: '#FFF'}} value={searchPokemon} placeholder="Search" placeholderTextColor="#fff" onChangeText={(value) => setSearchPokemon(value)}/>
       </SearchBar>
 
+      <FlatList
+        data={pokemonList}
+        renderItem={renderPokemon}
+      />
+
     </Container>
   );
 }
 
 export default Home;
-
-
