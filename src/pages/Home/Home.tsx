0b34f18b2ccb1d86e58bfb4b693d955ca1ae5ad8
Ariel Conti <arielconti10@gmail.com>
2021-03-30 10:52:09 -0300

    feat: search results and render only the searched pokemon

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,12 +7,13 @@ import { capitalize } from '../../helpers';
 import api from '../../services/api';
 
 import { Container, SearchBar, Title } from './styles';
-import { DebouncedFunc } from 'lodash';
+import { DebouncedFunc, lowerCase } from 'lodash';
 
 interface Pokemon{
   name: string,
   url: string
 }
+
 
 function useDebounce<T>(
   callback: any,
@@ -35,14 +36,29 @@ const Home: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false);
 
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [searchedPokemon, setSearchedPokemon] = useState([]);
+  const [searchedPokemon, setSearchedPokemon] = useState<Pokemon>();
 
   const getPokemons = () => {
+    setLoading(true);
     api.get(`pokemon?limit=10&offset=${offset}`).then(
       (result) => {
         setPokemonList(pokemonList => pokemonList.concat(result.data.results));
+        setLoading(false);
       }
     );
+  }
+
+  const searchPokemon = () => {
+    setLoading(true);
+    const query = lowerCase(debouncedSearch);
+    if(query){
+      setTimeout(() => {
+        api.get(`pokemon/${query}`).then(result => {
+          setSearchedPokemon(result.data);
+          setLoading(false);
+        }).catch(error => console.log(error));
+      }, 1000)
+    }
   }
 
   const renderPokemon = (result: { item: Pokemon }) => (
@@ -62,14 +78,6 @@ const Home: React.FC = () => {
       <Text>{capitalize(result.item.name)}</Text>
     </TouchableOpacity>
   );
-
-  const searchPokemon = () => {
-    if(debouncedSearch){
-      api.get(`pokemon/${debouncedSearch}`).then(result => {
-        console.log(result.data)
-      }).catch(error => console.log(error));
-    }
-  }
 
   const handleChangeSearch = (value: string) => {
     setSearchString(value);
@@ -99,19 +107,28 @@ const Home: React.FC = () => {
         />
       </SearchBar>
 
-      <FlatList
-        keyExtractor={(item) => item.name}
-        data={pokemonList}
-        renderItem={renderPokemon}
-        onEndReachedThreshold={0}
-        onEndReached={() => {
-          setTimeout(() => {
-            setLoading(true);
-            setOffset(offset + 10);
-          }, 1500)
-        }}
-        ListFooterComponent={() => <ActivityIndicator />}
-      />
+      {loading && <ActivityIndicator />}
+
+      {!searchedPokemon ? (
+        <FlatList
+          keyExtractor={(item) => item.name}
+          data={pokemonList}
+          renderItem={renderPokemon}
+          onEndReachedThreshold={0}
+          onEndReached={() => {
+            setTimeout(() => {
+              setLoading(true);
+              setOffset(offset + 10);
+            }, 1500)
+          }}
+          ListFooterComponent={() => <ActivityIndicator />}
+        />
+        ) :
+        null
+      }
+
+      {searchedPokemon ? renderPokemon({item: searchedPokemon}) : null}
+
 
     </Container>
   );
