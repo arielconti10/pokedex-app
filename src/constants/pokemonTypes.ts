aa9cc02e528073bec5f5bc2eb7fe23cf2fd13244
Ariel Conti <arielconti10@gmail.com>
2021-04-02 11:54:22 -0300

    feat: added all pokemon types colors

--- src/constants/pokemonTypes.ts
+++ src/constants/pokemonTypes.ts
@@ -6,6 +6,17 @@ enum pokemonColorTypes {
   grass = '#5DBE62',
   poison = '#B563CE',
   flying = '#9BB4E8',
+  normal = '#9A9DA1',
+  dark = '#5F606D',
+  dragon = '#0773C7',
+  fairy = '#EF97E6',
+  fight = '#D94256',
+  ghost = '#6970C5',
+  ground = '#D78555',
+  ice = '#7ED4C9',
+  psychic = '#F87C7A',
+  rock = '#CEC18C',
+  steel = '#5596A4',
 }
 
 export { pokemonColorTypes };
